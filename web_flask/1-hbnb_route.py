efbebb1c66cf9c73381754c4a1e642265fa106f5
obianuju23 <obianujunmoh@gmail.com>
2023-07-23 13:30:20 -0700

    script that starts a Flask web application:listening on 0.0.0.0, port 5000

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 
+This Scripts starts Flask web application on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """Shows the route what to display"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
+
