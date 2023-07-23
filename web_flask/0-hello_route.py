08c59dd55d3a5521605369b1f4f7247fa365951c
obianuju23 <obianujunmoh@gmail.com>
2023-07-23 14:03:06 -0700

    script that starts a Flask web application:

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ 
-This Scripts starts Flask web application on 0.0.0.0, port 5000
+This Scripts starts Flask web
+application on 0.0.0.0, port 5000
 """
 from flask import Flask
 app = Flask(__name__)
