d6ac9a31996f0fe2e4296be659eb2eabea870e3b
obianuju23 <obianujunmoh@gmail.com>
2023-07-23 12:54:17 -0700

    script that starts a Flask web application:

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" This Scripts starts Flask web application on 0.0.0.0, port 5000"""
+""" 
+This Scripts starts Flask web application on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False:)
+
+@app.route('/')
 def hello_hbnb():
     """Shows the route what to display"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
