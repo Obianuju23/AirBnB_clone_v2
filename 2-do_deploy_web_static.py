c441e2cbd5354deffbb83935ddbf540a7df6b1cc
Obianuju23 <obianujunmoh@gmail.com>
2023-07-29 07:55:31 -0700

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,31 +4,62 @@ Module that generates a .tgz archive from the
 web_static directory
 """
 
-from os.path import exists
-from fabric.api import env, run, put
+import os.path as osp
+from fabric.api import env
+from fabric.api import run
+from fabric.api import put
 
-
-env.hosts = ['100.26.253.193', '54.237.112.101']
+"""setting the environment host for the servers"""
+env.hosts = ['34.229.72.181', '54.84.154.151']
 
 
 def do_deploy(archive_path):
-    """A function that distributes an archive file to a web server"""
-    if not exists(archive_path):
-        return False
-    try:
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_name.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        print("New version deployed!")
-        flag = True
-    except Exception as e:
-        flag = False
-    return flag
+    """
+    A function that distributes an archive file to a web server
+
+    Args:
+        archive_path (str): path to the archive to distribute
+
+    Returns:
+        True if the archive path exist and there is no error
+        Otherwise - False
+    """
+    if osp.isfile(archive_path) is False:
+        return (False)
+    file = archive_path.split("/")[-1]
+    fn = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return (False)
+
+    if run("sudo rm -rf /data/web_static/releases/{}/".
+            format(fn)).failed is True:
+        return (False)
+
+    if run("sudo mkdir -p /data/web_static/releases/{}/".
+            format(fn)).failed is True:
+        return (False)
+
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, fn)).failed is True:
+        return (False)
+
+    if run("sudo rm /tmp/{}".format(file)).failed is True:
+        return (False)
+
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(fn, fn)).failed is True:
+        return (False)
+
+    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
+           format(fn)).failed is True:
+        return (False)
+
+    if run("sudo rm -rf /data/web_static/current").failed is True:
+        return (False)
+
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(fn)).failed is True:
+        return (False)
+
+    return True
